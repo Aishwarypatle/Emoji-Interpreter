c471d98f8c95dd54a4411ebe06f95c9e97319ed6
Aishwary Patle <61194375+Aishwarypatle@users.noreply.github.com>
2022-09-18 20:56:27 +0000

    Js code added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,57 @@
 import './App.css'
+import { useState } from 'react';
+
+
+let emoList = {
+  "😂": "Laughing";
+  "😎": "Smart";
+  "😴": "Sleepy";
+  "😣": "Pathetic";
+  "🙄": "Confused";
+  "😘": "Love";
+  "😙": "Kiss";
+  "😉": "Wink";
+  "😍": "Excited";
+  "😓": "Sad";
+  "✋": "High-Five";
+  "😑": "Annoyed";
+};
+
+let emoKeys = Object.keys(emoList);
 
 export default function App() {
-  return (
-    
+
+  let [describeEmo, setEmoDescribe] = useState("");
+
+  function emojiHandler() {
+    let input = event.target.value;
+
+    let describeEmo = emoList[input];
+
+    if (describeEmo === undefined) {
+      describeEmo = "Database don't have this particular Emoji,Please try again with different Emoji";
+    }
+
+    setEmoDescribe(describeEmo);
+  }
+
+  function emojiClickEvent(emoji) {
+    setEmoDescribe(emoList[emoji]);
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+  return
+  (
+ 
   )
 }
